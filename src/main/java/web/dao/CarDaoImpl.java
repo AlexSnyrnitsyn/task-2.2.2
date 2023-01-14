94cd196762c2e6eb104c7467c0966ec76ee1fd78
alexandr_snurnitsyn <alexsnurnitsyn@mail.ru>
2023-01-14 18:48:16 +0600

    fix code

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -11,13 +11,15 @@ public class CarDaoImpl implements CarDao {
 
     private static final ArrayList<Car> cars = new ArrayList<>();
 
-    public static List<Car> getAllCars() {
+    static {
         cars.add(new Car(1L, "Lada1", 1));
         cars.add(new Car(2L, "Lada2", 2));
         cars.add(new Car(3L, "Lada3", 3));
         cars.add(new Car(4L, "Lada4", 4));
         cars.add(new Car(5L, "Lada5", 5));
-        List<Car> carList = cars;
-        return carList;
+    }
+
+    public static List<Car> getAllCars() {
+        return cars;
     }
 }
